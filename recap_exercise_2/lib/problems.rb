67a5999966dc7ffa135d797c9b2632eb6ffa5036
Luis <perezluis167@gmail.com>
2022-11-17 12:08:53 -0500

    Removed zip file

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -65,6 +65,7 @@ class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
 
+        
 
 
     end
